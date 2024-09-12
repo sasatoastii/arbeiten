2902e4d35520e1b9e89deb99c62e1fd5bfc282ba
sasatoastii <33104502+sasatoastii@users.noreply.github.com>
2024-09-12 12:05:36 +0200

    Create blacksmithcraftig.lua

---
+++ blacksmithcraftig.lua
@@ -0,0 +1,186 @@
+---------------------------------
+-- blacksmith crafting
+---------------------------------
+Config.BlacksmithCrafting = {
+
+    {
+        category = 'Tools',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 1 },
+            [2] = { item = 'steelbar', amount = 1 },
+            [3] = { item = 'wood', amount = 1 },
+        },
+        receive = 'pickaxe',
+        giveamount = 1
+    },
+    {
+        category = 'Tools',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 1 },
+            [2] = { item = 'steelbar', amount = 1 },
+            [3] = { item = 'wood', amount = 1 },
+        },
+        receive = 'shovel',
+        giveamount = 1
+    },
+    {
+        category = 'Tools',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 2 },
+            [2] = { item = 'steelbar', amount = 2 },
+        },
+        receive = 'bucket',
+        giveamount = 1
+    },
+    {
+        category = 'Items',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 2 },
+            [2] = { item = 'steelbar', amount = 2 },
+        },
+        receive = 'handcuffs',
+        giveamount = 1
+    },
+    {
+        category = 'Items',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 4 },
+            [2] = { item = 'steelbar', amount = 3 },
+            [3] = { item = 'copperbar', amount = 1 },
+        },
+        receive = 'moneybox',
+        giveamount = 1
+    },
+    {
+        category = 'Bars',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 10 },
+            [2] = { item = 'goldore', amount = 10 },
+        },
+        receive = 'goldbar',
+        giveamount = 1
+    },
+    {
+        category = 'Bars',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 10 },
+            [2] = { item = 'silverore', amount = 10 },
+        },
+        receive = 'silverbar',
+        giveamount = 1
+    },
+    {
+        category = 'Bars',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 10 },
+            [2] = { item = 'ironore', amount = 10 },
+        },
+        receive = 'ironbar',
+        giveamount = 1
+    },
+    {
+        category = 'Bars',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 10 },
+            [2] = { item = 'ironbar', amount = 1 },
+        },
+        receive = 'steelbar',
+        giveamount = 1
+    },
+    {
+        category = 'Bars',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 10 },
+            [2] = { item = 'leadore', amount = 10 },
+        },
+        receive = 'leadbar',
+        giveamount = 1
+    },
+    {
+        category = 'Bars',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 10 },
+            [2] = { item = 'copperore', amount = 10 },
+        },
+        receive = 'copperbar',
+        giveamount = 1
+    },
+    {
+        category = 'Weapon Parts',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 1 },
+            [2] = { item = 'steelbar', amount = 1 },
+            [3] = { item = 'wood', amount = 1 },
+        },
+        receive = 'revolverparts',
+        giveamount = 1
+    },
+    {
+        category = 'Weapon Parts',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 1 },
+            [2] = { item = 'steelbar', amount = 1 },
+            [3] = { item = 'wood', amount = 1 },
+        },
+        receive = 'pistolparts',
+        giveamount = 1
+    },
+    {
+        category = 'Weapon Parts',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 2 },
+            [2] = { item = 'steelbar', amount = 2 },
+            [3] = { item = 'wood', amount = 2 },
+        },
+        receive = 'repeaterparts',
+        giveamount = 1
+    },
+    {
+        category = 'Weapon Parts',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 2 },
+            [2] = { item = 'steelbar', amount = 2 },
+            [3] = { item = 'wood', amount = 2 },
+        },
+        receive = 'rifleparts',
+        giveamount = 1
+    },
+    {
+        category = 'Weapon Parts',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 2 },
+            [2] = { item = 'steelbar', amount = 2 },
+            [3] = { item = 'wood', amount = 2 },
+        },
+        receive = 'shotgunparts',
+        giveamount = 1
+    },
+    {
+        category = 'Ammo Parts',
+        crafttime = 30000,
+        ingredients = { 
+            [1] = { item = 'coal', amount = 2 },
+            [2] = { item = 'copperbar', amount = 1 },
+            [3] = { item = 'leadbar', amount = 1 },
+        },
+        receive = 'ammoparts',
+        giveamount = 100
+    },
+
+}
